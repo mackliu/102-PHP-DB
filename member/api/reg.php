75a7c6bbd981259aaab5f3b6170a52277767a799
mackliu <macklun@gmail.com>
2021-11-15 16:29:03 +0800

    完成會員登入帳號檢查的功能

--- member/api/reg.php
+++ member/api/reg.php
@@ -1,7 +1,7 @@
 <?php
 
 $account=$_POST['account'];
-$password=$_POST['password'];
+$password=md5($_POST['password']);
 $mail=$_POST['mail'];
 
 $name=$_POST['name'];
@@ -16,14 +16,17 @@ $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
 
 
-echo $sql_account;
+/* echo $sql_account;
 echo "<hr>";
 echo $sql_member;
 echo "<hr>";
-echo $pdo->exec($sql_account);
-echo "<hr>";
-echo $pdo->exec($sql_member);
+echo "<hr>"; */
 
+//執行寫入資料表的動作
+$pdo->exec($sql_account);
+$pdo->exec($sql_member);
+
+header("location:../index.php");
 
 
 ?>
