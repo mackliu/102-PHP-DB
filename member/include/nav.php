f1da9dc16626d8ed7284760d07132c6fb64bc06d
mackliu <macklun@gmail.com>
2021-11-19 14:52:04 +0800

    增加刪除帳號功能

--- member/include/nav.php
+++ member/include/nav.php
@@ -2,12 +2,22 @@
 if(isset($_SESSION['user'])){
 ?>
   
+  <style>
+    .del{
+            background:grey;
+            color:#EEE;
+            float:right;
+    }
+
+  </style>
 <nav>
         <a href="index.php">回首頁</a>
         <a href="dashboard.php">會員中心</a>
         <a href="chang_pw.php">修改密碼</a>
         <a href="edit_user.php">編輯會員資料</a>
         <a href="logout.php">登出</a>
+        <a class="del" href="./api/del_user.php">刪除帳號</a>
+
 </nav>
 <?php  }else{  ?>
 <nav>
