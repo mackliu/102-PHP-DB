5e5361c87af2dd2e9fbc660f937812d897d954d8
mackliu <macklun@gmail.com>
2021-11-22 16:32:23 +0800

    使用不定參數來改良all函式

--- function/db.php
+++ function/db.php
@@ -11,9 +11,27 @@
     }
 
     //取出指定資料表的所有資料
-function all($table){
+function all($table,...$arg){
     global $pdo;
-    $sql="SELECT * FROM `$table`";
+    $sql="SELECT * FROM `$table` ";
+    if(isset($arg[0])){
+        if(is_array($arg[0])){
+            foreach($arg[0] as $key=>$value){
+                $tmp[]="`$key`='$value'";
+            }
+            
+            $sql=$sql."where " . implode(" AND ",$tmp);
+        }else{
+            $sql=$sql.$arg[0];
+        }
+    }
+
+    if(isset($arg[1])){
+        $sql=$sql.$arg[1];
+    }
+    
+    echo $sql;
+    
     $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
     //return $pdo->query($sql)->fetchAll();
