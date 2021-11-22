abb997469d268454b1a325161a54e79a0578eb59
mackliu <macklun@gmail.com>
2021-11-22 10:51:17 +0800

    使用函式來取得資料表的全部資料

---
+++ function/all.php
@@ -0,0 +1,25 @@
+<?php
+
+/* echo "<pre>";
+print_r(all('expenditure'));
+echo "</pre>"; */
+
+$rows=all('expenditure');
+
+
+foreach($rows as $row){
+    echo $row['item']."-".$row['place']."<br>";
+}
+
+
+function all($table){
+    $dsn="mysql:host=localhost;charset=utf8;dbname=students";
+    $pdo=new PDO($dsn,'root','');
+    $sql="SELECT * FROM `$table`";
+    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+    //return $pdo->query($sql)->fetchAll();
+}
+
+
+?>
